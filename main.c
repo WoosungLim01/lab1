e02da552aab3439fdc9c360d9a912e5370fd7b37
Woosung Lim <wml5207@psu.edu>
2020-09-03 18:43:52 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+// Author: Woosung Lim wml5207@psu.edu
+// Collaborator: Saeed Alshebli saa6016@psu.edu
+// Collaborator: Hunter Fristick hjf5143@psu.edu
+// Collaborator: Aisan O'connor avo5337@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *temp = readline("Enter temperature in Celsius: ");
+  
+double fahrenheit = atof(temp);
+ fahrenheit = fahrenheit*9/5+32;
+
+  printf("%f° in Celsius is equivalent to %f\n°Fahrenheit.\n", atof(temp),fahrenheit);
+
+  return 0;
+}
