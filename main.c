c6e6262e11a17cfe18a18973c510333ed71b8bc7
Woosung Lim <wml5207@psu.edu>
2020-09-03 18:49:12 +0000

    degree diff

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(void) {
 double fahrenheit = atof(temp);
  fahrenheit = fahrenheit*9/5+32;
 
-  printf("%f° in Celsius is equivalent to %f\n°Fahrenheit.\n", atof(temp),fahrenheit);
+  printf("%f° in Celsius is equivalent to %f°Fahrenheit.\n", atof(temp),fahrenheit);
 
   return 0;
 }
